749b8f09554a094bd9933d30540e9e3159535989
nakhyun <skrgus0140@naver.com>
2026-01-30 14:49:09 +0900

    조퇴용

--- WS/Opc/OPCSocketMiddleware.cs
+++ WS/Opc/OPCSocketMiddleware.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Concurrent;
 using System.Net.WebSockets;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using R.O.S.C.H.WS.Opc.DTO;
 
 namespace R.O.S.C.H.WS.Opc;
@@ -12,7 +15,7 @@ public class OPCSocketMiddleware(
     private readonly ILogger<OPCSocketMiddleware> _logger = logger;
     private readonly RequestDelegate _next = next;
     private static readonly ConcurrentDictionary<string, OpcClientDto> _clients = new ConcurrentDictionary<string, OpcClientDto>();
-
+    private static readonly ConcurrentDictionary<string, OpcClientDto> _unity = new ConcurrentDictionary<string, OpcClientDto>();
     public async Task InvokeAsync(HttpContext context)
     {
         if (!context.WebSockets.IsWebSocketRequest) await _next(context);
@@ -23,9 +26,69 @@ public class OPCSocketMiddleware(
         OpcClientDto client =  new OpcClientDto(clientType, socket);
         
         // dto 생성시 만들어지는 clientId로 client 등록
-        _clients.TryAdd(client.ClientId, client);
-        _logger.LogDebug("OPC client connected");
+        if (clientType.Equals("unity"))
+        {
+            _unity.TryAdd(client.ClientId, client);
+        }
+        else
+        {
+            _clients.TryAdd(client.ClientId, client);
+        }
+    }
+
+    public async Task SendDataAsync(IDictionary<string, object> sendData)
+    {
+        JObject obj = JObject.FromObject(sendData);
         
+        // client에는 컨베이어 속도만 전송
+        JObject sendToClient = new JObject();
+        sendToClient.Add("conv_load", obj["stm_stm_yolo_currentspeedload"].ToString());
+        sendToClient.Add("conv_main", obj["stm_stm_yolo_currentspeedmain"].ToString());
+        sendToClient.Add("conv_sort", obj["stm_stm_yolo_currentspeedsort"].ToString());
+
+        foreach (var client in _clients)
+        {
+            try
+            {
+                var sock = client.Value.Socket;
+
+                if (sock.State != WebSocketState.Open)
+                {
+                    _clients.TryRemove(client.Key, out _);
+                    continue;
+                }
+
+                var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(sendToClient));
+                await client.Value.Socket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true,
+                    CancellationToken.None);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"[OPCSocketMiddleware] client {client.Key} 에게 데이터 전송 중 오류 : " + e.Message);
+            }
+        }
+        
+        // unity에는 전체 데이터 전송
+        foreach (var client in _unity)
+        {
+            try
+            {
+                var sock = client.Value.Socket;
+
+                if (sock.State != WebSocketState.Open)
+                {
+                    _clients.TryRemove(client.Key, out _);
+                    continue;
+                }
+
+                var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj));
+                await client.Value.Socket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true,
+                    CancellationToken.None);
+            } catch (Exception e)
+            {
+                _logger.LogError($"[OPCSocketMiddleware] unity {client.Key} 에게 데이터 전송 중 오류 : " + e.Message);
+            }
+        }
         
     }
 }
